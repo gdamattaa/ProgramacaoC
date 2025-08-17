a48b4d6f026a509eb989911179862931355eccdb
Gabriel da Matta <damatta02@gmail.com>
2025-08-17 21:58:42 +0000

    código consertado, programa funcionando.

--- Jogo de Maior, Menor ou Igual.c
+++ Jogo de Maior, Menor ou Igual.c
@@ -13,18 +13,18 @@ numeroComputador = rand() %100 + 1;  // Número entre 1 e 100
 // Início do jogo
 printf("Bem vindo ao jogo Maior, Menor ou Igual!\n");
 printf("Você deve escolher um número e o tipo de comparação.\n");
-printf("M. Maior\n");
-printf("N. Menor\n");
-printf("I. Igual\n");
+printf("M para Maior\n");
+printf("N para Menor\n");
+printf("I para Igual\n");
 
 printf("Escolha a comparação:\n");
-scanf(" %c, &tipoComparacao");
+scanf("%c", &tipoComparacao);
 
-printf("Digite seu número (entre 1 e 100):\n");
-scanf("%d, &numeroJogador");
+printf("Digite seu número (entre 1 e 100): ");
+scanf("%d", &numeroJogador);
 
 // Exibir número do computador
-printf("O número do computador é: %d\n", numeroComputador);
+//printf("O número do computador é: %d\n", numeroComputador);
 
 switch (tipoComparacao)
 {
@@ -32,7 +32,6 @@ case 'M':
 case 'm':
     printf("Você escolheu a opção maior.\n");
     resultado = numeroJogador > numeroComputador ? 1 : 0;
-    
     break;
 case 'N':
 case 'n':
@@ -43,24 +42,20 @@ case 'I':
 case 'i':
     printf("Você escolheu a opção igual.\n");
     resultado = numeroJogador == numeroComputador ? 1 : 0;
-    break; 
+    break;
 default:
     printf("Opção de jogo inválida.\n");
     break;
 }
 
-printf("O número do computador é: %d e o do jogador é: %d\n", numeroComputador, numeroJogador);
+printf("O número do computador é: %d\n", numeroComputador);
 
 if (resultado == 1)
 {
     printf("Parabéns! Você venceu!\n");
-} else
-{
+} else {
     printf("Infelizmente você perdeu!\n");
 }
-
-
-
 
 
 }
